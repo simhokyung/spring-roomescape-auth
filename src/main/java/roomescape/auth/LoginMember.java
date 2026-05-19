dc570f76e57aa2844b736f2eef17e7424baa7b98
심호경 <ghrudtla@naver.com>
2026-05-19 20:56:02 +0900

    feat: 로그인 세션 모델 추가

---
+++ src/main/java/roomescape/auth/LoginMember.java
@@ -0,0 +1,8 @@
+package roomescape.auth;
+
+public record LoginMember(
+        Long id,
+        String name,
+        String email
+) {
+}
---
+++ src/main/java/roomescape/auth/SessionConst.java
@@ -0,0 +1,9 @@
+package roomescape.auth;
+
+public final class SessionConst {
+
+    public static final String LOGIN_MEMBER = "loginMember";
+
+    private SessionConst() {
+    }
+}
