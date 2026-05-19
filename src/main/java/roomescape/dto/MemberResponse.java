448b39600ff572038721fdcdb98201214f51019f
심호경 <ghrudtla@naver.com>
2026-05-19 20:47:11 +0900

    feat: 회원가입 요청과 응답 DTO 추가

---
+++ src/main/java/roomescape/dto/MemberResponse.java
@@ -0,0 +1,18 @@
+package roomescape.dto;
+
+import roomescape.domain.Member;
+
+public record MemberResponse(
+        Long id,
+        String name,
+        String email
+) {
+
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getEmail()
+        );
+    }
+}
