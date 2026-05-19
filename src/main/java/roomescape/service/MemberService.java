31070e756d78232467b907f687d605201741f3a5
심호경 <ghrudtla@naver.com>
2026-05-19 20:59:49 +0900

    feat: 회원 로그인 서비스 추가

--- src/main/java/roomescape/service/MemberService.java
+++ src/main/java/roomescape/service/MemberService.java
@@ -3,6 +3,7 @@ package roomescape.service;
 import org.springframework.stereotype.Service;
 import roomescape.dao.MemberDao;
 import roomescape.domain.Member;
+import roomescape.exception.AuthenticationException;
 import roomescape.exception.DuplicateResourceException;
 
 @Service
@@ -24,5 +25,16 @@ public class MemberService {
 
         Member member = new Member(name, email, password);
         return memberDao.save(member);
+    }
+
+    public Member login(String email, String password) {
+        Member member = memberDao.findByEmail(email)
+                .orElseThrow(() -> new AuthenticationException("이메일 또는 비밀번호가 올바르지 않습니다."));
+
+        if (!member.getPassword().equals(password)) {
+            throw new AuthenticationException("이메일 또는 비밀번호가 올바르지 않습니다.");
+        }
+
+        return member;
     }
 }
