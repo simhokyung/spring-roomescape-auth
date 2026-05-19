bc006b0e4310d5c0cce2dc2de70f3bd2cbfeb3c7
심호경 <ghrudtla@naver.com>
2026-05-19 20:49:18 +0900

    feat: 회원 저장 기능 추가

--- src/main/java/roomescape/dao/MemberDao.java
+++ src/main/java/roomescape/dao/MemberDao.java
@@ -1,10 +1,14 @@
 package roomescape.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Member;
 
+import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
@@ -57,4 +61,37 @@ public class MemberDao {
 
         return result.stream().findFirst();
     }
+
+    public Member save(Member member) {
+        String sql = """
+              INSERT INTO member (name, email, password)
+              VALUES (?, ?, ?)
+              """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, member.getName());
+            ps.setString(2, member.getEmail());
+            ps.setString(3, member.getPassword());
+            return ps;
+        }, keyHolder);
+
+        Long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
+
+        return new Member(
+                id,
+                member.getName(),
+                member.getEmail(),
+                member.getPassword()
+        );
+    }
+
+    public boolean existsByEmail(String email) {
+        String sql = "SELECT count(*) FROM member WHERE email = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+        return count != null && count > 0;
+    }
+
 }
