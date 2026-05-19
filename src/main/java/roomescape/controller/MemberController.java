79f33cebb567eb63e7ff0dc9279f48f4cf206d21
심호경 <ghrudtla@naver.com>
2026-05-19 20:51:06 +0900

    feat: 회원가입 API 추가

---
+++ src/main/java/roomescape/controller/MemberController.java
@@ -0,0 +1,34 @@
+package roomescape.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Member;
+import roomescape.dto.MemberCreateRequest;
+import roomescape.dto.MemberResponse;
+import roomescape.service.MemberService;
+
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/members")
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberResponse join(@Valid @RequestBody MemberCreateRequest request) {
+        Member member = memberService.join(
+                request.name(),
+                request.email(),
+                request.password()
+        );
+
+        return MemberResponse.from(member);
+    }
+}
