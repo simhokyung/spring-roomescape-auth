b766a7ab7c1ba40b34ff43bb62d3b4cf52f22f0a
심호경 <ghrudtla@naver.com>
2026-05-21 23:28:33 +0900

    feat: 예약 매장 소유 관계 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS store;
 DROP TABLE IF EXISTS reservation_time;
 DROP TABLE IF EXISTS theme;
 DROP TABLE IF EXISTS member;
@@ -11,6 +13,24 @@ CREATE TABLE member
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
+);
+
+CREATE TABLE store
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE manager
+(
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL,
+    store_id  BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (store_id) REFERENCES store (id),
+    UNIQUE (member_id)
 );
 
 CREATE TABLE reservation_time
@@ -34,12 +54,14 @@ CREATE TABLE reservation
 (
     id        BIGINT       NOT NULL AUTO_INCREMENT,
     member_id BIGINT       NOT NULL DEFAULT 1,
+    store_id  BIGINT       NOT NULL DEFAULT 1,
     name      VARCHAR(255),
     date      VARCHAR(255) NOT NULL,
     time_id   BIGINT,
     theme_id  BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (store_id)  REFERENCES store (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -4,5 +4,17 @@ VALUES ('brown', 'brown@example.com', 'password');
 INSERT INTO member (name, email, password)
 VALUES ('coney', 'coney@example.com', 'password');
 
+INSERT INTO store (name)
+VALUES ('gangnam');
+
+INSERT INTO store (name)
+VALUES ('jamsil');
+
+INSERT INTO manager (member_id, store_id)
+VALUES (1, 1);
+
+INSERT INTO manager (member_id, store_id)
+VALUES (2, 2);
+
 -- 이 파일은 테스트 시에 로드되어 기존 하드코딩된 테스트가 실패하는 것을 막기 위해 존재.
 SELECT 1;
